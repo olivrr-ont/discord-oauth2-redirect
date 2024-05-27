fb1af2966adf412d0dafdc030842cac2867ebf34
OlivrrDEV <137780419+olivrr-ont@users.noreply.github.com>
2024-05-27 18:23:37 +0300

    Update callback.js

--- functions/callback.js
+++ functions/callback.js
@@ -6,7 +6,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   payload.append('client_id', process.env.1244629352801636462)
   payload.append('client_secret', process.env.MfFMIf_g_MstlUXrBlyTv0Vh3UgmTt4E)
   payload.append('grant_type', 'authorization_code')
-  payload.append('redirect_uri', process.env.https://olivrr-ont.github.io/discord-oauth2-redirect/)
+  payload.append('redirect_uri', process.env.'https://olivrr-ont.github.io/discord-oauth2-redirect/')
   payload.append('code', accessCode)
   payload.append('scope', 'bot')
   
